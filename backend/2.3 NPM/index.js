baae67fdb0809236246bfc7d551a8fdcb97a8271
Brandon Hayes <106934444+iToastMost@users.noreply.github.com>
2024-02-05 14:11:49 -0500

    Add files via upload

---
+++ backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// //import generateName from "sillyname";
+// import generateName from "superheroes";
+// var sillyName = generateName();
+
+// console.log(`I am ${sillyName}!!`);
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+
+console.log(`I am ${name}!`);
